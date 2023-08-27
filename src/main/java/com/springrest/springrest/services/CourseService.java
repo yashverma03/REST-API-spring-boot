8a4e44dbe8d7abd134b34a4b08d7951124ec0895
yashverma03 <vermayash2003@gmail.com>
2023-08-27 11:50:33 +0530

    added REST API

--- src/main/java/com/springrest/springrest/services/CourseService.java
+++ src/main/java/com/springrest/springrest/services/CourseService.java
@@ -10,4 +10,8 @@ public interface CourseService {
   public Course getCourse(long courseId);
 
   public Course addCourse(Course course);
+
+  public Course updateCourse(Course course);
+
+  public void deleteCourse(long parseLong);
 }
--- src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -2,6 +2,7 @@ package com.springrest.springrest.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,7 @@ public class CourseServiceImpl implements CourseService {
   public CourseServiceImpl() {
     list = new ArrayList<>();
     list.add(new Course(145, "Java Core Course", "this course contains basics of Java"));
-    list.add(new Course(4343, "Spring boot Course", "Ccreating rest api using spring boot"));
+    list.add(new Course(4343, "Spring boot Course", "creating rest api using spring boot"));
   }
 
   @Override
@@ -40,5 +41,22 @@ public class CourseServiceImpl implements CourseService {
   public Course addCourse(Course course) {
     list.add(course);
     return course;
+  }
+
+  @Override
+  public Course updateCourse(Course course) {
+    list.forEach(e -> {
+      if (e.getId() == course.getId()) {
+        e.setTitle(course.getTitle());
+        e.setDescription(course.getDescription());
+      }
+    });
+
+    return course;
+  }
+
+  @Override
+  public void deleteCourse(long parseLong) {
+    list = this.list.stream().filter(e -> e.getId() != parseLong).collect(Collectors.toList());
   }
 }
